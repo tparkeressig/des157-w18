5e79ca9025a0d7d461d46d63139a57736faf5d73
Tara Parker-Essig <tparkeressig@ucdavis.edu>
2018-01-18 01:13:00 -0800

    Tidy cat animation with white bg

--- script.js
+++ script.js
@@ -7,19 +7,27 @@ console.log("Hey you linked that js alright!");
 function setup() {
     var myCanvas = createCanvas(800, 250); //capture the createCanvas to a variable so you can infuence it with CSS
     myCanvas.parent('mySketch'); //parent the myCanvas var to the mySketch id for CSS
-}
+    loadImage('sleep-cat.png', function(img) {
+            image(img, 300, 50);
+        });
+    background(250); //color
 
-function draw() { //put your interaction here meow meow
-    if (mouseIsPressed) {
-        background(250, 50); //color, alpha
-        loadImage('purr-cat.png', function(img) {
-            image(img, 300, 50);
-        });
-    } else {
-        background(250, 50); //color, alpha
-        loadImage('sleep-cat.png', function(img) {
-            image(img, 300, 50);
-        });
     }
-    print(mouseIsPressed);
-}
+
+    function draw() { //put your interaction here meow meow
+        var d = dist(mouseX, mouseY, 400, 80);
+        if (d < 50) {
+            if (mouseIsPressed) {
+                background(250, 80); //color, alpha
+                loadImage('purr-cat.png', function(img) {
+                    image(img, 300, 50);
+                });
+            } else {
+                background(250, 80); //color, alpha
+                loadImage('sleep-cat.png', function(img) {
+                    image(img, 300, 50);
+                });
+            }
+        }
+        print(mouseIsPressed);
+    }
