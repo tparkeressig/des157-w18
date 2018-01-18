a9e101f09149c0392b28d52378f91ea2bbc032d9
Tara Parker-Essig <tparkeressig@ucdavis.edu>
2018-01-18 00:46:08 -0800

    Two Cats
    Purring and sleeping

--- script.js
+++ script.js
@@ -2,21 +2,15 @@
 
 console.log("Hey you linked that js alright!");
 
-var count;
-
 function setup() {
     var myCanvas = createCanvas(800, 250); //capture the createCanvas to a variable so you can infuence it with CSS
     myCanvas.parent('mySketch'); //parent the myCanvas var to the mySketch id for CSS
-
-    // loadImage('sleep-cat.png', function(img) {
-    //     image(img, 300, 50);
-    // });
 }
 
 function draw() { //put your interaction here meow meow
     if (mouseIsPressed) {
-        background(0, 50); //color, alpha
-        loadImage('sleep-cat.png', function(img) {
+        background(250, 50); //color, alpha
+        loadImage('purr-cat.png', function(img) {
             image(img, 300, 50);
         });
     } else {
