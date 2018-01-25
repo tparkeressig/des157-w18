59f5e635323ada9838aff31cb2a4d3c84dc8f73c
Tara Parker-Essig <tparkeressig@ucdavis.edu>
2018-01-25 03:18:44 -0800

    Fixed purring cat!
    It works great! So happy!

--- script.js
+++ script.js
@@ -4,38 +4,45 @@
 
 console.log("Hey you linked that js alright!");
 
+var d; //for distance later
+
 function preload() {
-  soundFormats('mp3', 'ogg');
-  catPurr = loadSound('bigboy.mp3');
+    soundFormats('mp3', 'ogg');
+    catPurr = loadSound('bigboy.mp3');
 }
 
-function setup() {
+function setup() { //once
     var myCanvas = createCanvas(800, 250); //capture the createCanvas to a variable so you can infuence it with CSS
     myCanvas.parent('mySketch'); //parent the myCanvas var to the mySketch id for CSS
     loadImage('sleep-cat.png', function(img) {
+        image(img, 300, 50);
+    });
+    background(250); //color
+}
+
+function draw() { //repeating
+}
+
+function mousePressed() { //when you press down on the mouse
+    var d = dist(mouseX, mouseY, 410, 120);
+    if (d < 75) {
+        background(250, 250); //color, alpha
+        loadImage('purr-cat.png', function(img) { //loving cat
             image(img, 300, 50);
         });
-    background(250); //color
-
+        catPurr.play();
+        catPurr.playMode('sustain');
+        print("Should be purring.");
     }
+    print("You clicked");
+    print(d);
+}
 
-    function draw() { //put your interaction here meow meow
-        var d = dist(mouseX, mouseY, 400, 80);
-        if (d < 50) {
-            if (mouseIsPressed) {
-                background(250, 250); //color, alpha
-                loadImage('purr-cat.png', function(img) { //loving cat
-                    image(img, 300, 50);
-                });
-                catPurr.play();
-                catPurr.playMode('untilDone');
-            } else {
-                background(250, 250); //color, alpha
-                loadImage('sleep-cat.png', function(img) { //sleeping cat
-                    image(img, 300, 50);
-                });
-                catPurr.stop();
-            }
-        }
-        print(mouseIsPressed);
+function mouseReleased() {
+    background(250, 250); //color, alpha
+    loadImage('sleep-cat.png', function(img) { //sleeping cat
+            image(img, 300, 50);
+        });
+        catPurr.stop();
+        print("Should stop purring.");
     }
