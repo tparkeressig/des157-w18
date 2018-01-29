4ea2d2fa62867250abde127507a6b09c74ec0928
Tara Parker-Essig <tparkeressig@ucdavis.edu>
2018-01-29 00:27:50 -0800

    Basic script up and running!
    Woohoo! One variable is a-go!

--- studio1/script.js
+++ studio1/script.js
@@ -2,4 +2,21 @@
     Tara Parker-Essig
     DES 157 WQ 2018  */
 
-console.log("Hello!");
+console.log("Hello! We're ready to go!");
+
+// to capture form submission and send it to a function
+document.fillThisForm.onsubmit = processForm;
+
+//to capture the clear event and send it to a function
+document.fillThisForm.onreset = clearForm;
+
+function processForm() {
+    var noun1 = document.fillThisForm.noun1.value;
+    console.log(noun1);
+    //prevent page from reloading
+    return false;
+}
+
+function clearForm() {
+    console.log("Form cleared!");
+}
